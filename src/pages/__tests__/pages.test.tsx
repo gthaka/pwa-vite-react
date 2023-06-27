87193b7794119441d90270439909b2a81d98b780
Anthony G <agithaka@live.com>
2023-06-27 19:27:27 +0300

    pages tests

--- src/pages/__tests__/pages.test.tsx
+++ src/pages/__tests__/pages.test.tsx
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { escapeRegExp } from "lodash";
 import Home from "../Home";
+import About from "../About";
+import Users from "../Users";
+import { APP_NAME } from "../../application";
+import { MemoryRouter } from "react-router-dom";
 
-describe("Home Component", () => {
+describe("Home Page", () => {
   test("renders with into text", () => {
     render(
       <MemoryRouter>
@@ -14,5 +18,23 @@ describe("Home Component", () => {
     expect(
       screen.getByText(/a simple pwa implementation/i)
     ).toBeInTheDocument();
+  });
+});
+
+describe("About Page", () => {
+  test("renders with about head text", () => {
+    render(<About />);
+
+    const appNameRegex = new RegExp(escapeRegExp(`about ${APP_NAME}`), "i");
+    expect(screen.getByText(appNameRegex)).toBeInTheDocument();
+  });
+});
+
+describe("Users Page", () => {
+  test("renders with into text", () => {
+    render(<Users />);
+
+    // Assert that theuers text is present
+    expect(screen.getByText(/users/i)).toBeInTheDocument();
   });
 });
